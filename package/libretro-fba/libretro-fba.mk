797f52829b34026d5ebe8a80ea8fbe963cfad412
Yann Morère <yann.morere@univ-lorraine.fr>
2016-11-18 08:20:49 +0100

    last FBA

--- package/libretro-fba/libretro-fba.mk
+++ package/libretro-fba/libretro-fba.mk
@@ -3,12 +3,8 @@
 # FBA
 #
 ################################################################################
-<<<<<<< HEAD
-#LIBRETRO_FBA_VERSION = d5a2be323e334035cbfd471439d42c7957c5a99d
-LIBRETRO_FBA_VERSION = b9ef7c1349c720de6ce6242a710a306cf12dc35d
-=======
-LIBRETRO_FBA_VERSION = 43d71c74c2f5d8c47296baacf6e86cfde0bbed9a
->>>>>>> 518de218baf5a14e8550aafb331081cc78dff52b
+#LIBRETRO_FBA_VERSION = 43d71c74c2f5d8c47296baacf6e86cfde0bbed9a
+LIBRETRO_FBA_VERSION = 50cda51cc95d6c028e09e73a9dfb1c3833090496
 LIBRETRO_FBA_SITE = $(call github,libretro,fbalpha,$(LIBRETRO_FBA_VERSION))
 
 define LIBRETRO_FBA_BUILD_CMDS

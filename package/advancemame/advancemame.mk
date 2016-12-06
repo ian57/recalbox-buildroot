f4ff62c49ddf381952a0d93c065ac5ffabcfb061
Yann Morère <yann.morere@univ-lorraine.fr>
2016-12-06 09:27:25 +0100

    bump to advmame 3.0

--- package/advancemame/advancemame.mk
+++ package/advancemame/advancemame.mk
@@ -7,13 +7,17 @@
 #latest
 #ADVANCEMAME_VERSION = 339eab5a4b19a614b53efee2d62015c758694a4f
 #stable
-ADVANCEMAME_VERSION = advancemame-1.4
-ADVANCEMAME_SITE = $(call github,amadvance,advancemame,$(ADVANCEMAME_VERSION))
+#ADVANCEMAME_VERSION = advancemame-1.4
+ADVANCEMAME_VERSION = 3.0
+#https://github.com/amadvance/advancemame/releases/download/v3.0/advancemame-3.0.tar.gz
+
+#ADVANCEMAME_SITE = $(call github,amadvance,advancemame,$(ADVANCEMAME_VERSION))
+ADVANCEMAME_SITE = https://github.com/amadvance/advancemame/releases/download/v$(ADVANCEMAME_VERSION)
 ADVANCEMAME_DEPENDENCIES += rpi-userland sdl
 
-define ADVANCEMAME_RUN_AUTOGEN
-        cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
-endef
+#define ADVANCEMAME_RUN_AUTOGEN
+#        cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+#endef
 
 ADVANCEMAME_PRE_CONFIGURE_HOOKS += ADVANCEMAME_RUN_AUTOGEN
 
@@ -27,8 +31,6 @@ ADVANCEMAME_CONF_OPTS += \
 	--prefix=$(TARGET_DIR)/usr \
 	--enable-sdl --with-sdl-prefix=$(STAGING_DIR)/usr \
 	--enable-freetype --with-freetype-prefix=$(STAGING_DIR)/usr \
-	--disable-kraw \
-	--disable-kevent \
 	--enable-alsa \
 	--disable-oss
 
